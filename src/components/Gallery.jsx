8e381d678a1d109ef9dc41a8a58086a5a45fff14
Maxime Doré <maximedore43@gmail.com>
2022-09-03 20:47:02 -0400

    Side tabs style

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -15,16 +15,30 @@ const Gallery = () => {
 
 	return (
 		<section id="galerie" className="bg-black">
-			<div className="gallery__tabs absolute left-0 top-[var(--padd)] bottom-[var(--padd)] z-[9]">
-				<div className="sticky top-[150px]">
-					<button type="button" className={`gallery__button`} onClick={disableUnsplash}>
-						<span className="button__text"></span>
-						<span className="button__line">Galerie</span>
-					</button>
-					<button type="button" className={`gallery__button`} onClick={enableUnsplash}>
-						<span className="button__text"></span>
-						<span className="button__line">Unsplash</span>
-					</button>
+			<div className="absolute w-0 left-0 top-[var(--padd)] bottom-[var(--padd)] z-[9]">
+				<div className="sticky top-[100px]">
+					<div className="gallery__tabs flex">
+						<button
+							type="button"
+							className={`gallery__button button button--white button--sideways ${
+								!isUnsplash ? "button--active" : ""
+							}`}
+							onClick={disableUnsplash}
+						>
+							<span className="button__line"></span>
+							<span className="button__text">Galerie</span>
+						</button>
+						<button
+							type="button"
+							className={`gallery__button button button--white button--sideways ${
+								isUnsplash ? "button--active" : ""
+							}`}
+							onClick={enableUnsplash}
+						>
+							<span className="button__text">Unsplash</span>
+							<span className="button__line"></span>
+						</button>
+					</div>
 				</div>
 			</div>
 			<div className="wrap padd overflow-hidden">
