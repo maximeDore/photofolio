335cd91cceda118bcd475b100450eb3b329032fb
Maxime Doré <maximedore43@gmail.com>
2022-09-06 15:24:14 -0400

    Sort by date and added footer image separator
    - Added older images to the gallery

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,26 @@
-import { logo } from "../assets";
+import { footerSeparator } from "../assets";
 import { socialMedia } from "../constants";
 
 const Footer = () => (
-	<footer className="wrap bg-white text-black relative">
-		<div className="w-full wrapper flex justify-between items-center md:flex-row flex-col pt-6 pb-6">
-			<p>© Maxime Doré, Tous droits réservés {new Date().getFullYear()} </p>
-
-			{/* TODO: Ajouter un disclaimer */}
-
-			<div className="flex flex-row md:mt-0 mt-6">
-				{socialMedia.map((social, index) => (
-					<a key={social.id} href={social.link} title={social.name} target="_blank">
-						<img
-							src={social.icon}
-							alt={social.id}
-							className={`social w-[21px] h-[21px] object-contain cursor-pointer ${
-								index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
-							}`}
-						/>
-					</a>
-				))}
+	<footer className="bg-white text-black relative">
+		<img src={footerSeparator} width="1920" height="433" className="w-full" alt="" />
+		<div className="wrap">
+			<div className="w-full wrapper flex justify-between items-center md:flex-row md:text-left text-center flex-col pb-6">
+				<p>© Maxime Doré, Tous droits réservés {new Date().getFullYear()} </p>
+				{/* TODO: Ajouter un disclaimer */}
+				<div className="flex flex-row md:mt-0 mt-6">
+					{socialMedia.map((social, index) => (
+						<a key={social.id} href={social.link} title={social.name} target="_blank">
+							<img
+								src={social.icon}
+								alt={social.id}
+								className={`social w-[21px] h-[21px] object-contain cursor-pointer ${
+									index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+								}`}
+							/>
+						</a>
+					))}
+				</div>
 			</div>
 		</div>
 	</footer>
